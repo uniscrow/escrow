50124b736279428d0653658dc60c6281ce3ffd89
davide <davide@Mini-di-davide.lan>
2022-02-05 15:02:56 +0100

    test token for solc 0.8

--- test/factory.js
+++ test/factory.js
@@ -7,7 +7,7 @@ describe("WalletFactory", function () {
     before(async function () {
         this.Factory = await ethers.getContractFactory("Factory");
         this.ERC20 = await ethers.getContractFactory("ERC20");
-        [this.alice, this.bob, this.charlie, this.dave, this.backup] = await ethers.getSigners();
+        [this.alice, this.bob, this.charlie, this.dave, this.backup, this.agent] = await ethers.getSigners();
         //console.log(this.alice.address);
         this.factory = await this.Factory.deploy();
         await this.factory.deployed();
@@ -17,18 +17,31 @@ describe("WalletFactory", function () {
         console.log("token:"+this.token.address);
     });
     
-    it("A new wallet is created", async function () {
+    it("A new wallet with backup is created", async function () {
         
         await expect(this.factory
-            .create2out3Backup([this.alice.address,
+            .create2out3Backup(
+                            this.alice.address,
                             this.bob.address,
-                            this.dave.address],
+                            this.dave.address,
                             this.backup.address,
                             this.token.address))
             .to
             .emit(this.factory,"NewWallet");
             //.withArgs();
-        
+    });
 
+    it("A new wallet with managing agent is created", async function () {
+        
+        await expect(this.factory
+            .create2out3Managed(
+                            this.alice.address,
+                            this.bob.address,
+                            this.dave.address,
+                            this.agent.address,
+                            this.token.address))
+            .to
+            .emit(this.factory,"NewWallet");
+            //.withArgs();
     });
 });
