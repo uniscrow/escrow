e14d97f15fc2804e3676d3aa9de0fd067c33757f
Davide <davide@Air-di-Davide.lan>
2022-07-19 00:40:25 +0200

    modified factory contract

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.8 <0.9.0;
 
 import "./JointWalletArbitrated.sol";
-import "./JointWalletManaged.sol";
 
 contract Factory{
     event NewWallet(address);
@@ -20,21 +19,5 @@ contract Factory{
         emit NewWallet(address(wallet));
 
     }
-
-    function createManaged(
-                            address alice,
-                            address bob,
-                            address agent, 
-                            address erc20)  
-                            external{
-        
-        JointWalletManaged wallet =
-        new JointWalletManaged( 
-                                    alice, 
-                                    bob, 
-                                    agent,
-                                    erc20);
-        emit NewWallet(address(wallet));
-
-    }    
+  
 }
