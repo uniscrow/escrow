38dbbc34024a740e79ea1566f820dc07037d80d2
davide <davide@Mini-di-davide.lan>
2022-11-28 14:50:58 +0100

    new escrow structure

---
+++ contracts/EscrowTransaction.sol
@@ -0,0 +1,59 @@
+pragma solidity >=0.8 <0.9.0;
+
+
+interface IERC20Transfer {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+contract EscrowTransaction{
+   
+    event Released(uint256);
+    event Refunded(uint256);
+    event Settled(uint256, uint256);
+    
+    address public buyer;
+    address public seller;
+    address public arbitrator;
+
+
+    IERC20Transfer public token;
+    
+    constructor(
+        address _buyer, 
+        address _seller, 
+        address _arbitrator,
+        address erc20){
+        //no address can be null
+        require (_buyer != address(0));
+        require (_seller   != address(0));
+        require (_arbitrator != address(0));
+
+        require (erc20  != address(0));
+        
+        buyer = _buyer;
+        seller   = _seller;
+        arbitrator = _arbitrator;
+        token = IERC20Transfer(erc20);
+    }
+    
+
+    function release(uint256 amount) external{
+        require(msg.sender == buyer, "Only buyer can release funds");
+        token.transfer(seller, amount);
+        emit Released(amount);
+    }
+
+    function refund(uint256 amount) external{
+        require(msg.sender == seller, "Only seller can refund");
+        token.transfer(buyer, amount);
+        emit Refunded(amount);
+    }
+
+    function settle(uint256 refunded, uint256 released) external {
+        require(msg.sender == arbitrator, "Only arbitrator can settle" );
+        token.transfer(buyer, refunded);
+        token.transfer(seller, released);
+        emit Settled(refunded, released);
+    }
+    
+}
