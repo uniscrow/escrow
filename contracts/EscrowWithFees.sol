4bab1ab80cd7fb2de746fea9e065843b8c96d23e
davide <davide@Mini-di-davide.lan>
2022-11-29 17:04:56 +0100

    added missing files

---
+++ contracts/EscrowWithFees.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.8 <0.9.0;
+
+import "./EscrowTransaction.sol";
+import "./FeeCalculator.sol";
+
+
+
+contract EscrowWithFees is EscrowTransaction, FeeCalculator{
+
+    constructor(
+        address _buyer,
+        address _seller,
+        address _arbitrator,
+        address _feeRecipient,
+        uint256 _flatFee,
+        uint256 _bps,
+        address erc20
+    ) 
+    EscrowTransaction(_buyer, _seller, _arbitrator, erc20) 
+    FeeCalculator(_feeRecipient, _flatFee, _bps){}
+
+    function _transfer(address to, uint256 amount) internal override {
+        uint256 fee = calculateFee(amount);
+        super._transfer(feeRecipient,fee);
+        super._transfer(to, amount );
+    }
+
+}
