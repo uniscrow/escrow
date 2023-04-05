2ff87386e30552c79eab3020a7c0e61e47dfcbf8
davide <davide@Mini-di-davide.lan>
2023-04-05 18:26:37 +0200

    allowance param now in createEscrow

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,7 +6,6 @@ contract Factory{
     address public arbitrator;
     address public erc20;
     address public feeRecipient;
-    uint256 public allowance;
     
 
     event EscrowCreated(address);
@@ -14,19 +13,19 @@ contract Factory{
     constructor(
         address _arbitrator, 
         address _erc20,
-        address _feeRecipient,
-        uint256 _allowance){
+        address _feeRecipient){
             arbitrator = _arbitrator;
             erc20 = _erc20;
             feeRecipient = _feeRecipient;
-            allowance = _allowance;
+
     }
 
     function createEscrow(
         address seller, 
         address buyer, 
         uint256 flatFee, 
-        uint256 bpsFee ) external{
+        uint256 bpsFee,
+        uint256 allowance ) external{
 
         PresetManagedWithFees escrow =
             new PresetManagedWithFees(
