e6f0773052b28b1b3d82d5a1878a9bc490267d81
Giovanni Casu <giovanni.casu@gmail.com>
2023-04-20 11:21:37 +0200

    id7-fix

--- contracts/EscrowWithFees.sol
+++ contracts/EscrowWithFees.sol
@@ -3,10 +3,22 @@ pragma solidity 0.8.18;
 import "./EscrowTransaction.sol";
 import "./FeeCalculator.sol";
 
+/**
+ * @title EscrowWithFees contract for handling escrow transactions with fees.
+ * @author Davide Carboni
 
-
-contract EscrowWithFees is EscrowTransaction, FeeCalculator{
-
+ */
+contract EscrowWithFees is EscrowTransaction, FeeCalculator {
+    /**
+     * @notice Creates a new EscrowWithFees contract.
+     * @param _buyer The address of the buyer in the escrow contract.
+     * @param _seller The address of the seller in the escrow contract.
+     * @param _arbitrator The address of the arbitrator.
+     * @param _feeRecipient The address of the recipient of the escrow fees.
+     * @param _flatFee The flat fee amount to be paid to the fee recipient.
+     * @param _bps The fee amount in basis points to be paid to the fee recipient.
+     * @param erc20 The address of the ERC20 token used for payments.
+     */
     constructor(
         address _buyer,
         address _seller,
@@ -17,12 +29,17 @@ contract EscrowWithFees is EscrowTransaction, FeeCalculator{
         address erc20
     ) 
     EscrowTransaction(_buyer, _seller, _arbitrator, erc20) 
-    FeeCalculator(_feeRecipient, _flatFee, _bps){}
+    FeeCalculator(_feeRecipient, _flatFee, _bps) {}
 
+    /**
+     * @notice Transfers the specified amount to the recipient and deducts the calculated fee.
+     * @dev Overrides the _transfer function from the EscrowTransaction contract.
+     * @param to The address of the recipient.
+     * @param amount The amount to be transferred.
+     */
     function _transfer(address to, uint256 amount) internal override {
         uint256 fee = calculateFee(amount);
-        super._transfer(feeRecipient,fee);
-        super._transfer(to, amount );
+        super._transfer(feeRecipient, fee);
+        super._transfer(to, amount);
     }
-
 }
--- contracts/PresetManagedWithFees.sol
+++ contracts/PresetManagedWithFees.sol
@@ -3,8 +3,25 @@ pragma solidity 0.8.18;
 import "./EscrowWithFees.sol";
 import "./Managed.sol";
 
-contract PresetManagedWithFees is EscrowWithFees,Managed {
-        constructor(
+/**
+ * @title PresetManagedWithFees contract that combines EscrowWithFees and Managed contracts.
+ * @author Davide Carboni
+
+ * @dev This contract inherits from both EscrowWithFees and Managed contracts.
+ */
+contract PresetManagedWithFees is EscrowWithFees, Managed {
+    /**
+     * @notice Creates a new PresetManagedWithFees contract.
+     * @param _buyer The address of the buyer in the escrow contract.
+     * @param _seller The address of the seller in the escrow contract.
+     * @param _arbitrator The address of the arbitrator.
+     * @param _feeRecipient The address of the recipient of the escrow fees.
+     * @param _flatFee The flat fee amount to be paid to the fee recipient.
+     * @param _bps The fee amount in basis points to be paid to the fee recipient.
+     * @param erc20 The address of the ERC20 token used for payments.
+     * @param allowance The maximum amount of tokens that the agent is allowed to spend.
+     */
+    constructor(
         address _buyer,
         address _seller,
         address _arbitrator,
@@ -13,13 +30,16 @@ contract PresetManagedWithFees is EscrowWithFees,Managed {
         uint256 _bps,
         address erc20,
         uint256 allowance
-    ) EscrowWithFees(
-        _buyer,
-        _seller, 
-        _arbitrator,
-        _feeRecipient,
-        _flatFee,
-        _bps, 
-        erc20)
-    Managed( _arbitrator, erc20, allowance){}
+    )
+        EscrowWithFees(
+            _buyer,
+            _seller,
+            _arbitrator,
+            _feeRecipient,
+            _flatFee,
+            _bps,
+            erc20
+        )
+        Managed(_arbitrator, erc20, allowance)
+    {}
 }
